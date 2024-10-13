c58a9ffd6ac4d1b11f4bcfa2c0382f1980bf1924
elg <elgthedev@gmail.com>
2024-10-13 20:19:17 -0300

    Release 1

---
+++ Plugin.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ItemManager;
+using JetBrains.Annotations;
+using LocalizationManager;
+using PieceManager;
+using ServerSync;
+using UnityEngine;
+
+namespace ZilheimPieces
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class ZilheimPiecesPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "ZilheimPieces";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "elg";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+        public static readonly ManualLogSource ZilheimPiecesLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+        private static readonly ConfigSync ConfigSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            // Uncomment the line below to use the LocalizationManager for localizing your mod.
+            Localizer.Load(); // Use this to initialize the LocalizationManager (for more information on LocalizationManager, see the LocalizationManager documentation https://github.com/blaxxun-boop/LocalizationManager#example-project).
+
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On, "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            // Globally turn off configuration options for your pieces, omit if you don't want to do this.
+            BuildPiece.ConfigurationEnabled = false;
+
+            Item hammer = new("zilheimpieces", "Zil_Hammer");
+            hammer.Snapshot();
+
+            // Format: new("AssetBundleName", "PrefabName", "FolderName");
+            BuildPiece zilHouse1 = new("zilheimpieces", "Zil_House1");
+
+            zilHouse1.Category.Set("Zilheim");
+            zilHouse1.Tool.Add("Zil_Hammer");
+            zilHouse1.Snapshot(); 
+            zilHouse1.SpecialProperties = new SpecialProperties() { AdminOnly = true, NoConfig = true }; // You can declare multiple properties in one line           
+
+            //zilHouse1.RequiredItems.Add("Wood", 20, false); // Add any requirements to build
+            //zilHouse1.Extension.Set(CraftingTable.Forge, 2); // Makes this piece a station extension, can change the max station distance by changing the second value. Use strings for custom tables.
+
+            //// Does your model need to swap materials with a vanilla material? Format: (GameObject, isJotunnMock)
+            //MaterialReplacer.RegisterGameObjectForMatSwap(zilHouse1.Prefab, false);
+            // Detailed instructions on how to use the MaterialReplacer can be found on the current PieceManager Wiki. https://github.com/AzumattDev/PieceManager/wiki
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                ZilheimPiecesLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                ZilheimPiecesLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                ZilheimPiecesLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription = new(description.Description + (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"), description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order = null!;
+            [UsedImplicitly] public bool? Browsable = null!;
+            [UsedImplicitly] public string? Category = null!;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer = null!;
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() => "# Acceptable values: " + string.Join(", ", UnityInput.Current.SupportedKeyCodes);
+        }
+
+        #endregion
+    }
+    
+    public static class KeyboardExtensions
+    {
+        public static bool IsKeyDown(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKeyDown(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
+        }
+
+        public static bool IsKeyHeld(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKey(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
+        }
+    }
+}
